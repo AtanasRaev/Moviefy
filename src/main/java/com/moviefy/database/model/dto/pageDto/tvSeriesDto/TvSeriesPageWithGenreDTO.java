cf8b215e6f0044a0eba03c6ab58376d66cf80cf2
AtanasRaev <atanaspraev@gmail.com>
2025-11-13 03:56:56 +0200

    Add apiId to TvSeriesPageWithGenreDTO constructor
    Updated the TvSeriesPageWithGenreDTO constructor to include the apiId parameter and pass it to the superclass. This change ensures consistency with the parent class and allows apiId to be set when creating instances.

--- src/main/java/com/moviefy/database/model/dto/pageDto/tvSeriesDto/TvSeriesPageWithGenreDTO.java
+++ src/main/java/com/moviefy/database/model/dto/pageDto/tvSeriesDto/TvSeriesPageWithGenreDTO.java
@@ -5,8 +5,8 @@ public class TvSeriesPageWithGenreDTO extends TvSeriesPageDTO {
 
     private String trailer;
 
-    public TvSeriesPageWithGenreDTO(Long id, Double voteAverage, Double popularity, String name, Integer seasonsCount, Integer episodesCount, String posterPath, Integer year, String genre, String trailer) {
-        super(id, voteAverage, popularity, name, seasonsCount, episodesCount, posterPath, year);
+    public TvSeriesPageWithGenreDTO(Long id, Double voteAverage, Double popularity, Long apiId, String name, Integer seasonsCount, Integer episodesCount, String posterPath, Integer year, String genre, String trailer) {
+        super(id, voteAverage, popularity, apiId, name, seasonsCount, episodesCount, posterPath, year);
         this.genre = genre;
         this.trailer = trailer;
     }
