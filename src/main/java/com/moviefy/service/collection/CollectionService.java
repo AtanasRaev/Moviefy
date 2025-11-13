26b96661bdc7109fc4ebe6b52215fcfda365f193
AtanasRaev <atanaspraev@gmail.com>
2025-11-13 20:33:02 +0200

    Update CollectionService interface with new methods
    Added methods for retrieving collections by name, fetching movies by API ID, getting popular collections with pagination, and obtaining movies for the home page by collection. These changes expand the service's capabilities to support new features and data retrieval patterns.

--- src/main/java/com/moviefy/service/collection/CollectionService.java
+++ src/main/java/com/moviefy/service/collection/CollectionService.java
@@ -1,10 +1,15 @@
 package com.moviefy.service.collection;
 
 import com.moviefy.database.model.dto.apiDto.CollectionApiDTO;
+import com.moviefy.database.model.dto.pageDto.movieDto.CollectionPageDTO;
+import com.moviefy.database.model.dto.pageDto.movieDto.MoviePageDTO;
 import com.moviefy.database.model.entity.media.Collection;
 import com.moviefy.database.model.entity.media.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CollectionService {
     void saveCollection(Collection collection);
@@ -17,5 +22,11 @@ public interface CollectionService {
 
     List<Collection> getByName(String name);
 
-    List<Collection> getByNames(List<String> name);
+    List<CollectionPageDTO> getCollectionsByName(List<String> input);
+
+    Map<String, List<MoviePageDTO>> getMoviesByApiId(Long apiId);
+
+    Page<CollectionPageDTO> getPopular(Pageable pageable);
+
+    Map<String, Object> getMoviesHomeByCollection(String input);
 }
