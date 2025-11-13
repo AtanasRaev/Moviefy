29682cdd192ef4a263193210144354ded483eee3
AtanasRaev <atanaspraev@gmail.com>
2025-11-13 20:33:28 +0200

    Replace MovieController with MovieCollectionController
    Removed MovieController and introduced MovieCollectionController to handle movie collection-related endpoints. The new controller provides improved endpoints for searching collections, retrieving collection details, and listing popular collections, with enhanced error handling and response structures.

---
+++ src/main/java/com/moviefy/web/MovieCollectionController.java
@@ -0,0 +1,118 @@
+package com.moviefy.web;
+
+import com.moviefy.database.model.dto.pageDto.movieDto.CollectionPageDTO;
+import com.moviefy.database.model.dto.pageDto.movieDto.MoviePageDTO;
+import com.moviefy.service.collection.CollectionService;
+import com.moviefy.utils.ErrorResponseUtil;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieCollectionController {
+    private final CollectionService collectionService;
+
+    public MovieCollectionController(CollectionService collectionService) {
+        this.collectionService = collectionService;
+    }
+
+    @GetMapping("/collection")
+    public ResponseEntity<Map<String, Object>> getCollectionMoviesSearch1(@RequestParam("name") String input) {
+        if (input == null || input.isBlank()) {
+            return ErrorResponseUtil.buildErrorResponse(
+                    HttpStatus.BAD_REQUEST,
+                    "Invalid request",
+                    "The search must not be empty!"
+            );
+        }
+
+        Map<String, Object> map = this.collectionService.getMoviesHomeByCollection(input);
+
+        if (map == null || map.isEmpty()) {
+            return ErrorResponseUtil.buildErrorResponse(
+                    HttpStatus.NOT_FOUND,
+                    "Resource not found",
+                    String.format("Not found collection '%s'", input)
+            );
+        }
+
+        return ResponseEntity.ok(map);
+    }
+
+    @GetMapping("/collections")
+    public ResponseEntity<Map<String, Object>> getCollectionSearch(@RequestParam("names") List<String> input) {
+        if (input == null || input.isEmpty()) {
+            return ErrorResponseUtil.buildErrorResponse(
+                    HttpStatus.BAD_REQUEST,
+                    "Invalid request",
+                    "The search must not be empty!"
+            );
+        }
+
+        List<CollectionPageDTO> collections = this.collectionService.getCollectionsByName(input);
+
+        if (collections == null || collections.isEmpty()) {
+            return ErrorResponseUtil.buildErrorResponse(
+                    HttpStatus.NOT_FOUND,
+                    "Resource not found",
+                    String.format("Not found collections '%s'", input)
+            );
+        }
+
+        return ResponseEntity.ok(Map.of(
+                "collections", collections)
+        );
+    }
+
+    @GetMapping("/collection/{apiId}")
+    public ResponseEntity<Map<String, Object>> getMovieDetails(@PathVariable Long apiId) {
+        Map<String, List<MoviePageDTO>> moviesByApiId = this.collectionService.getMoviesByApiId(apiId);
+
+        if (moviesByApiId == null || moviesByApiId.isEmpty()) {
+            return ErrorResponseUtil.buildErrorResponse(
+                    HttpStatus.NOT_FOUND,
+                    "Resource not found",
+                    String.format("Not found collection with apiId '%d'", apiId)
+            );
+        }
+
+        return ResponseEntity.ok(Map.of(
+                "collection_name", moviesByApiId.entrySet().iterator().next().getKey(),
+                "movies", moviesByApiId.entrySet().iterator().next().getValue()
+        ));
+    }
+
+    @GetMapping("/collections/popular")
+    public ResponseEntity<Map<String, Object>> getPopularCollections(
+            @RequestParam(defaultValue = "10") @Min(4) @Max(100) int size,
+            @RequestParam(defaultValue = "1") @Min(1) int page) {
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<CollectionPageDTO> popular = this.collectionService.getPopular(pageable);
+
+        if (popular == null || popular.isEmpty()) {
+            return ErrorResponseUtil.buildErrorResponse(
+                    HttpStatus.NOT_FOUND,
+                    "Resource not found",
+                    "There are no collections"
+            );
+        }
+
+        return ResponseEntity.ok(Map.of(
+                "items_on_page", popular.getNumberOfElements(),
+                "total_items", popular.getTotalElements(),
+                "total_pages", popular.getTotalPages(),
+                "current_page", popular.getNumber() + 1,
+                "popular", popular.getContent()
+        ));
+    }
+}
