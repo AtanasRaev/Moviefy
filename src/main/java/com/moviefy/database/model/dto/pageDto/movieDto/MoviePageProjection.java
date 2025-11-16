707f62a236198eb434c514c66d6499d222aca963
AtanasRaev <atanaspraev@gmail.com>
2025-11-17 00:35:27 +0200

    Add `MoviePageProjection` and `TvSeriesPageProjection` DTOs
    Introduced projection interfaces for movies and TV series page data with proper JSON annotations. These DTOs enhance response structure and align with API requirements by including relevant fields and ignoring unnecessary ones.

---
+++ src/main/java/com/moviefy/database/model/dto/pageDto/movieDto/MoviePageProjection.java
@@ -0,0 +1,23 @@
+package com.moviefy.database.model.dto.pageDto.movieDto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+public interface MoviePageProjection {
+    Long getId();
+    @JsonProperty("api_id")
+    Long getApiId();
+    String getTitle();
+    Double getPopularity();
+    @JsonProperty("poster_path")
+    String getPosterPath();
+    @JsonProperty("vote_average")
+    Double getVoteAverage();
+    Integer getYear();
+    String getType();
+    Integer getRuntime();
+    @JsonIgnore
+    LocalDate getReleaseDate();
+}
---
+++ src/main/java/com/moviefy/database/model/dto/pageDto/tvSeriesDto/TvSeriesPageProjection.java
@@ -0,0 +1,26 @@
+package com.moviefy.database.model.dto.pageDto.tvSeriesDto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+public interface TvSeriesPageProjection {
+    Long getId();
+    @JsonProperty("api_id")
+    Long getApiId();
+    String getName();
+    Double getPopularity();
+    @JsonProperty("poster_path")
+    String getPosterPath();
+    @JsonProperty("vote_average")
+    Double getVoteAverage();
+    Integer getYear();
+    String getType();
+    @JsonProperty("seasons")
+    Integer getSeasonsCount();
+    @JsonProperty("episodes")
+    Integer getEpisodesCount();
+    @JsonIgnore
+    LocalDate getReleaseDate();
+}
