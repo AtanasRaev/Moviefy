7473375fc74aabe259374f36e951988715371d98
AtanasRaev <atanaspraev@gmail.com>
2025-11-17 00:34:30 +0200

    Replace `CombinedMediaProjection` with `MediaProjection`
    Removed `CombinedMediaProjection` and introduced `MediaProjection` with enhanced field annotations for JSON property mapping and ignored fields. Improved DTO clarity and alignment with API response requirements.

---
+++ src/main/java/com/moviefy/database/model/dto/pageDto/MediaProjection.java
@@ -0,0 +1,27 @@
+package com.moviefy.database.model.dto.pageDto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+public interface MediaProjection {
+    Long getId();
+    @JsonProperty("api_id")
+    Long getApiId();
+    String getTitle();
+    Double getPopularity();
+    @JsonProperty("poster_path")
+    String getPosterPath();
+    @JsonProperty("vote_average")
+    Double getVoteAverage();
+    Integer getYear();
+    String getType();
+    @JsonProperty( "seasons")
+    Integer getSeasonsCount();
+    @JsonProperty("episodes")
+    Integer getEpisodesCount();
+    Integer getRuntime();
+    @JsonIgnore
+    LocalDate getReleaseDate();
+}
