306608f980ac90b4795d2659d68530d7bb4ce658
AtanasRaev <atanaspraev@gmail.com>
2025-11-17 00:36:10 +0200

    Add `findAllNames` query in `SeriesGenreRepository`
    Introduced a method to fetch all series genre names, enhancing repository functionality for genre-related operations.

--- src/main/java/com/moviefy/database/repository/genre/SeriesGenreRepository.java
+++ src/main/java/com/moviefy/database/repository/genre/SeriesGenreRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -15,4 +16,7 @@ public interface SeriesGenreRepository extends JpaRepository<SeriesGenre, Long> 
 
     @Query("SELECT sg FROM SeriesGenre sg JOIN sg.tvSeries tv WHERE tv.id = :tvSeriesId")
     Set<SeriesGenre> findByTvSeriesId(@Param("tvSeriesId") Long tvSeriesId);
+
+    @Query("SELECT sg.name FROM SeriesGenre sg")
+    List<String> findAllNames();
 }
