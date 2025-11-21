18246bd59c0293caecedca06a6811385a0c144ef
AtanasRaev <atanaspraev@gmail.com>
2025-11-21 19:40:22 +0200

    Rename `CastApiApiDTO` to `CastApiDTO` in `MediaResponseCreditsDTO` for naming consistency

--- src/main/java/com/moviefy/database/model/dto/apiDto/MediaResponseCreditsDTO.java
+++ src/main/java/com/moviefy/database/model/dto/apiDto/MediaResponseCreditsDTO.java
@@ -3,15 +3,15 @@ package com.moviefy.database.model.dto.apiDto;
 import java.util.Set;
 
 public class MediaResponseCreditsDTO {
-    Set<CastApiApiDTO> cast;
+    Set<CastApiDTO> cast;
 
     Set<CrewApiDTO> crew;
 
-    public Set<CastApiApiDTO> getCast() {
+    public Set<CastApiDTO> getCast() {
         return cast;
     }
 
-    public void setCast(Set<CastApiApiDTO> cast) {
+    public void setCast(Set<CastApiDTO> cast) {
         this.cast = cast;
     }
 
