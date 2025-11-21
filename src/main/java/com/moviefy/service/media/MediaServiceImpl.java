96d96f8ad65334afae56997a2cb59a4433c822e2
AtanasRaev <atanaspraev@gmail.com>
2025-11-21 22:38:17 +0200

    Refactor `MediaServiceImpl` to process genres using `genreNormalizationUtil` and implement `getTrendingMedia` with caching and updated projection

--- src/main/java/com/moviefy/service/media/MediaServiceImpl.java
+++ src/main/java/com/moviefy/service/media/MediaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.moviefy.service.media;
 
 import com.moviefy.database.model.dto.pageDto.MediaProjection;
+import com.moviefy.database.model.dto.pageDto.MediaWithGenreProjection;
 import com.moviefy.database.repository.media.MediaRepository;
 import com.moviefy.service.genre.movieGenre.MovieGenreService;
 import com.moviefy.service.genre.seriesGenre.SeriesGenreService;
@@ -56,29 +57,26 @@ public class MediaServiceImpl implements MediaService {
             unless = "#result == null || #result.isEmpty()"
     )
     public Page<MediaProjection> getLatestMedia(List<String> genres, Pageable pageable) {
-
-        List<String> seriesGenres;
-        List<String> movieGenres;
-
-        if (genres == null || genres.isEmpty()) {
-            seriesGenres = this.seriesGenreService.getAllGenresNames();
-            movieGenres  = this.movieGenreService.getAllGenresNames();
-        } else {
-            seriesGenres = genres;
-            movieGenres  = genres;
-        }
-
-        List<String> lowerCaseSeriesGenres = this.genreNormalizationUtil.getSeriesLowerCaseGenres(seriesGenres);
-        List<String> lowerCaseMoviesGenres = movieGenres.stream()
-                .map(String::toLowerCase)
-                .toList();
+        List<String> lowerCaseMoviesGenres = this.genreNormalizationUtil.processMovieGenres(genres);
+        List<String> lowerCaseSeriesGenres = this.genreNormalizationUtil.processSeriesGenres(genres);
 
         return this.mediaRepository.findLatestMedia(getStartOfCurrentMonth(), lowerCaseMoviesGenres, lowerCaseSeriesGenres, pageable);
     }
 
     @Override
-    public Page<MediaProjection> getTrendingMedia(List<String> genres, Pageable pageable) {
-        return null;
+    @Cacheable(
+            cacheNames = "trendingMedia",
+            key = "T(java.util.Objects).toString(#genres) + " +
+                    "';p=' + #pageable.pageNumber + " +
+                    "';s=' + #pageable.pageSize + " +
+                    "';sort=' + T(java.util.Objects).toString(#pageable.sort)",
+            unless = "#result == null || #result.isEmpty()"
+    )
+    public Page<MediaWithGenreProjection> getTrendingMedia(List<String> genres, Pageable pageable) {
+        List<String> lowerCaseMoviesGenres = this.genreNormalizationUtil.processMovieGenres(genres);
+        List<String> lowerCaseSeriesGenres = this.genreNormalizationUtil.processSeriesGenres(genres);
+
+        return this.mediaRepository.findTrendingMedia(lowerCaseMoviesGenres, lowerCaseSeriesGenres, pageable);
     }
 
     private LocalDate getStartOfCurrentMonth() {
