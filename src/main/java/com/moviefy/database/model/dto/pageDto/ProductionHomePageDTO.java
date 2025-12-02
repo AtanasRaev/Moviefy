c28d09ec554b44248e8388816c6991bee060e5b6
AtanasRaev <atanaspraev@gmail.com>
2025-12-02 16:32:23 +0200

    Add id and logoPath fields to ProductionHomePageDTO
    Introduces 'id' and 'logoPath' properties with corresponding getters and setters to the ProductionHomePageDTO class, enabling storage and retrieval of production IDs and logo paths.

--- src/main/java/com/moviefy/database/model/dto/pageDto/ProductionHomePageDTO.java
+++ src/main/java/com/moviefy/database/model/dto/pageDto/ProductionHomePageDTO.java
@@ -1,7 +1,27 @@
 package com.moviefy.database.model.dto.pageDto;
 
 public class ProductionHomePageDTO {
+    private long id;
+
+    private String logoPath;
+
     private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogoPath() {
+        return logoPath;
+    }
+
+    public void setLogoPath(String logoPath) {
+        this.logoPath = logoPath;
+    }
 
     public String getName() {
         return name;
