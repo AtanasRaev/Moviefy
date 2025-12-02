aac8dade9d140bcbfaecae99328194e7fd939caf
AtanasRaev <atanaspraev@gmail.com>
2025-12-02 16:32:16 +0200

    Add id and profilePath fields to CrewHomePageDTO
    Introduced new fields 'id' and 'profilePath' to the CrewHomePageDTO class, along with their respective getters and setters. This enhances the DTO to carry additional crew member information.

--- src/main/java/com/moviefy/database/model/dto/pageDto/CrewHomePageDTO.java
+++ src/main/java/com/moviefy/database/model/dto/pageDto/CrewHomePageDTO.java
@@ -1,9 +1,21 @@
 package com.moviefy.database.model.dto.pageDto;
 
 public class CrewHomePageDTO {
+    private long id;
+
     private String name;
 
+    private String profilePath;
+
     private String job;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -11,6 +23,14 @@ public class CrewHomePageDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getProfilePath() {
+        return profilePath;
+    }
+
+    public void setProfilePath(String profilePath) {
+        this.profilePath = profilePath;
     }
 
     public String getJob() {
