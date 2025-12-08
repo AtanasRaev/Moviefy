45f29fd7e91ddb9e16819317c02444eaedf38079
AtanasRaev <atanaspraev@gmail.com>
2025-12-08 19:33:25 +0200

    Add equals and hashCode to MovieApiDTO
    Implements custom equals and hashCode methods in MovieApiDTO based on the id field to ensure proper comparison and hashing of MovieApiDTO instances.

--- src/main/java/com/moviefy/database/model/dto/apiDto/MovieApiDTO.java
+++ src/main/java/com/moviefy/database/model/dto/apiDto/MovieApiDTO.java
@@ -3,6 +3,7 @@ package com.moviefy.database.model.dto.apiDto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class MovieApiDTO extends MediaApiDTO {
     private String title;
@@ -14,6 +15,19 @@ public class MovieApiDTO extends MediaApiDTO {
     private LocalDate releaseDate;
 
     public MovieApiDTO() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MovieApiDTO that = (MovieApiDTO) o;
+        return Objects.equals(this.getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(getId());
     }
 
     public String getTitle() {
