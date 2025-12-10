0149afb852724683fa35cfaafb858340b1670d2e
AtanasRaev <atanaspraev@gmail.com>
2025-12-10 17:23:38 +0200

    Refactor CrewService interface to add `processTvSeriesCrew` and `processMovieCrew` methods, streamline crew processing logic, and improve method naming clarity.

--- src/main/java/com/moviefy/service/credit/crew/CrewService.java
+++ src/main/java/com/moviefy/service/credit/crew/CrewService.java
@@ -1,30 +1,22 @@
 package com.moviefy.service.credit.crew;
 
 import com.moviefy.database.model.dto.apiDto.CrewApiDTO;
-import com.moviefy.database.model.dto.apiDto.MediaResponseCreditsDTO;
 import com.moviefy.database.model.dto.pageDto.CrewPageDTO;
 import com.moviefy.database.model.entity.credit.crew.Crew;
+import com.moviefy.database.model.entity.media.Movie;
+import com.moviefy.database.model.entity.media.tvSeries.TvSeries;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public interface CrewService {
-    Set<Crew> mapToSet(List<CrewApiDTO> crewDto);
+    Set<Crew> filterAndSaveCrew(List<CrewApiDTO> crewDto);
 
     List<CrewApiDTO> filterCrewApiDto(Set<CrewApiDTO> crewDTO);
 
-    <T, E> void processCrew(
-            List<CrewApiDTO> crewDto,
-            T parentEntity,
-            Function<CrewApiDTO, Optional<E>> findFunction,
-            BiFunction<CrewApiDTO, T, E> entityCreator,
-            Function<E, E> saveFunction,
-            Function<CrewApiDTO, String> jobNameFunction,
-            Set<Crew> crewSet
-    );
+    void processTvSeriesCrew(Set<CrewApiDTO> crewDto, TvSeries tvSeries);
+
+    void processMovieCrew(Set<CrewApiDTO> crewDto, Movie movie);
 
     Set<CrewPageDTO> getCrewByMediaId(String mediaType, long mediaId);
 }
