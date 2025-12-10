a42ef98e39530027692030a28ac1cfecb9ea14a1
AtanasRaev <atanaspraev@gmail.com>
2025-12-10 17:23:04 +0200

    Add EntityComparator utility for comparing MediaApiDTO and Media entities
    Introduces `EntityComparator` class to compare `MediaApiDTO` and `Media` objects based on vote count, popularity, and ID for improved utility and clarity in entity comparison logic.IdResponseDTO and EntityComparator utility
    
    Introduces `MediaApiByIdResponseDTO` as an abstract DTO for API responses by ID with production companies and credits. Adds `EntityComparator` utility for comparing media entities based on vote count, popularity, and IDs with null safety in value handling.Add SeasonsService interface and MediaApiByIdResponseDTO
    
    Introduces `SeasonsService` interface for managing TV series seasons and episodes, and `MediaApiByIdResponseDTO` abstract class

---
+++ src/main/java/com/moviefy/service/media/tvSeries/seasons/SeasonsService.java
@@ -0,0 +1,19 @@
+package com.moviefy.service.media.tvSeries.seasons;
+
+import com.moviefy.database.model.dto.databaseDto.EpisodeDTO;
+import com.moviefy.database.model.dto.databaseDto.SeasonDTO;
+import com.moviefy.database.model.entity.media.tvSeries.SeasonTvSeries;
+import com.moviefy.database.model.entity.media.tvSeries.TvSeries;
+
+import java.util.List;
+import java.util.Set;
+
+public interface SeasonsService {
+    Set<SeasonTvSeries> mapSeasonsAndEpisodesFromResponse(List<SeasonDTO> seasonsDTO, TvSeries tvSeries);
+
+    Set<SeasonTvSeries> findAllByTvSeriesId(Long id);
+
+    List<EpisodeDTO> getEpisodesFromSeason(Long seasonId);
+
+    Integer getSeasonNumberById(Long seasonId);
+}
