dc9bbe5bbbaa0289fb85d72a109d394d32330cb9
AtanasRaev <atanaspraev@gmail.com>
2025-12-10 17:25:08 +0200

    Refactor `MovieServiceImpl` to streamline cast and crew processing by delegating directly to `CastService` and `CrewService`, and remove obsolete repositories, methods, and timestamp logic.

--- src/main/java/com/moviefy/service/productionCompanies/ProductionCompanyService.java
+++ src/main/java/com/moviefy/service/productionCompanies/ProductionCompanyService.java
@@ -1,5 +1,6 @@
 package com.moviefy.service.productionCompanies;
 
+import com.moviefy.database.model.dto.apiDto.MediaApiByIdResponseDTO;
 import com.moviefy.database.model.dto.databaseDto.ProductionCompanyDTO;
 import com.moviefy.database.model.entity.ProductionCompany;
 import com.moviefy.database.model.entity.media.Media;
@@ -8,7 +9,7 @@ import java.util.Map;
 import java.util.Set;
 
 public interface ProductionCompanyService {
-    Map<String, Set<ProductionCompany>> getProductionCompaniesFromResponse(Object responseById, Media type);
+    Map<String, Set<ProductionCompany>> getProductionCompaniesFromResponse(MediaApiByIdResponseDTO responseById, Media type);
 
     void saveAllProduction(Set<ProductionCompany> productionCompanies);
 
