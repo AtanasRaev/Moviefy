a7825fb260568be590992bb75bd485d8e6bbec1b
AtanasRaev <atanaspraev@gmail.com>
2025-12-10 17:23:19 +0200

    Refactor CastService interface to simplify method signatures and improve clarity

--- src/main/java/com/moviefy/service/credit/cast/CastService.java
+++ src/main/java/com/moviefy/service/credit/cast/CastService.java
@@ -3,37 +3,20 @@ package com.moviefy.service.credit.cast;
 import com.moviefy.database.model.dto.apiDto.CastApiDTO;
 import com.moviefy.database.model.dto.pageDto.CastPageDTO;
 import com.moviefy.database.model.entity.credit.cast.Cast;
+import com.moviefy.database.model.entity.media.Movie;
+import com.moviefy.database.model.entity.media.tvSeries.TvSeries;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public interface CastService {
-    Set<Cast> mapToSet(List<CastApiDTO> castDto);
+    Set<Cast> filterAndSave(List<CastApiDTO> castDto);
 
     List<CastApiDTO> filterCastApiDto(Set<CastApiDTO> castDTO);
 
-    <T, E> void processCast(
-            List<CastApiDTO> castDto,
-            T parentEntity,
-            Function<CastApiDTO, Optional<E>> findFunction,
-            BiFunction<CastApiDTO, T, E> entityCreator,
-            Function<E, E> saveFunction
-    );
+    void processTvSeriesCast(Set<CastApiDTO> castDto, TvSeries tvSeries);
 
-    <T, E> E createCastEntity(
-            CastApiDTO dto,
-            T parentEntity,
-            Set<Cast> castSet,
-            Supplier<E> entityCreator,
-            BiConsumer<E, T> setParentFunction,
-            BiConsumer<E, Cast> setCastFunction,
-            BiConsumer<E, String> setCharacterFunction
-    );
+    void processMovieCast(Set<CastApiDTO> castDto, Movie movie);
 
     Set<CastPageDTO> getCastByMediaId(String mediaType, long mediaId);
 }
