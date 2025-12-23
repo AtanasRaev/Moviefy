b19f269b9ddcd7336dd08f4d83dc911c174b75d5
AtanasRaev <atanaspraev@gmail.com>
2025-12-23 17:07:24 +0200

    Add `COOL_DOWN_DAYS` constant to `RefreshConfig` for configurable cooldown period.

--- src/main/java/com/moviefy/config/cache/RefreshConfig.java
+++ src/main/java/com/moviefy/config/cache/RefreshConfig.java
@@ -5,6 +5,7 @@ public final class RefreshConfig {
     public static final int TRENDING_CAP = 80;
     public static final int DAYS_CAP = 60;
     public static final int DAYS_GUARD = 3;
+    public static final int COOL_DOWN_DAYS = 10;
 
     private RefreshConfig() {}
 }
