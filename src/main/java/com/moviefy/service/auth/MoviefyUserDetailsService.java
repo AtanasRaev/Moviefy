ce8b6c4193e7d78fdf5d3d42ed225cb55d33bf7a
AtanasRaev <atanaspraev@gmail.com>
2026-01-03 20:38:44 +0200

    Add `MoviefyUserDetailsService` to load user details for authentication and role management.

---
+++ src/main/java/com/moviefy/service/auth/MoviefyUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.moviefy.service.auth;
+
+import com.moviefy.database.model.entity.user.AppUser;
+import com.moviefy.database.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MoviefyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MoviefyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser user = this.userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return User
+                .withUsername(user.getEmail())
+                .password(user.getPasswordHash())
+                .disabled(!user.isEmailVerified())
+                .authorities(user.getRoles().stream()
+                        .map(r -> new SimpleGrantedAuthority("ROLE_" + r.name()))
+                        .toList())
+                .build();
+    }
+}
