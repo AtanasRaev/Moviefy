74817b441795da434463aab7cd9cafd77e2f7b15
AtanasRaev <atanaspraev@gmail.com>
2026-01-03 20:37:54 +0200

    Add `AppUser` entity to represent application users with roles, favorite movies/series, and email verification status.Add `AuthController` and `AppUser` entity for user registration and email verification functionality

---
+++ src/main/java/com/moviefy/web/AuthController.java
@@ -0,0 +1,47 @@
+package com.moviefy.web;
+
+import com.moviefy.database.model.dto.databaseDto.user.EmailVerificationTokenDTO;
+import com.moviefy.database.model.dto.databaseDto.user.RegisterUserDTO;
+import com.moviefy.database.model.dto.response.ApiResponse;
+import com.moviefy.service.auth.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<Void>> register(@Valid @RequestBody RegisterUserDTO registerUserDTO) {
+        this.authService.register(registerUserDTO);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(
+                        HttpStatus.CREATED.value(),
+                        "Registration successful. Please check your email to verify your account.",
+                        null
+                ));
+    }
+
+    @PostMapping("/verify-email")
+    public ResponseEntity<ApiResponse<Void>> verifyEmail(@RequestBody EmailVerificationTokenDTO emailVerificationTokenDTO) {
+        this.authService.verifyEmail(emailVerificationTokenDTO);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success(
+                        HttpStatus.OK.value(),
+                        "Email verified successfully.",
+                        null
+                ));
+    }
+}
