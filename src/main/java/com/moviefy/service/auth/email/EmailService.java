a7a212850783606c0396438178ceb3474a581db7
AtanasRaev <atanaspraev@gmail.com>
2026-01-03 20:38:10 +0200

    Add `EmailAlreadyTakenException` to handle duplicate email registration scenarios.Add `EmailService` interface for email verification management and `EmailAlreadyTakenException` for handling duplicate email errors.

---
+++ src/main/java/com/moviefy/service/auth/email/EmailService.java
@@ -0,0 +1,16 @@
+package com.moviefy.service.auth.email;
+
+import com.moviefy.database.model.entity.user.AppUser;
+import com.moviefy.database.model.entity.user.EmailVerificationToken;
+
+import java.util.Optional;
+
+public interface EmailService {
+    void sendVerificationEmail(String to, String token);
+
+    String generateToken(AppUser user);
+
+    Optional<EmailVerificationToken> findValidToken(String token);
+
+    void delete(EmailVerificationToken emailVerificationToken);
+}
