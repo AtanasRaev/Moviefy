8eecd4fcf70b3679ecea714c8cc6b858f4858966
AtanasRaev <atanaspraev@gmail.com>
2026-01-03 20:38:27 +0200

    Add `EmailVerificationTokenDTO` for representing email verification tokens in the system.

---
+++ src/main/java/com/moviefy/database/model/dto/databaseDto/user/EmailVerificationTokenDTO.java
@@ -0,0 +1,13 @@
+package com.moviefy.database.model.dto.databaseDto.user;
+
+public class EmailVerificationTokenDTO {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
