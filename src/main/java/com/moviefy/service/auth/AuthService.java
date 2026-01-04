ec3396772316ee840b85577d5bf7f465ea5eee9c
AtanasRaev <atanaspraev@gmail.com>
2026-01-04 15:15:32 +0200

    Add password reset handling in `AuthService` with methods for initiating and confirming password resets, refactor email verification methods for clarity

--- src/main/java/com/moviefy/service/auth/AuthService.java
+++ src/main/java/com/moviefy/service/auth/AuthService.java
@@ -1,6 +1,8 @@
 package com.moviefy.service.auth;
 
 import com.moviefy.database.model.dto.databaseDto.user.EmailVerificationTokenDTO;
+import com.moviefy.database.model.dto.databaseDto.user.PasswordResetConfirmDTO;
+import com.moviefy.database.model.dto.databaseDto.user.PasswordResetRequestDTO;
 import com.moviefy.database.model.dto.databaseDto.user.RegisterUserDTO;
 
 public interface AuthService {
@@ -8,5 +10,9 @@ public interface AuthService {
 
     void verifyEmail(EmailVerificationTokenDTO emailVerificationTokenDTO);
 
-    void resendEmail(EmailVerificationTokenDTO emailVerificationTokenDTO);
+    void resendEmailVerification(EmailVerificationTokenDTO emailVerificationTokenDTO);
+
+    void requestPasswordReset(PasswordResetRequestDTO passwordResetRequestDTO);
+
+    void confirmPasswordReset(PasswordResetConfirmDTO passwordResetConfirmDTO);
 }
--- src/main/java/com/moviefy/service/auth/AuthServiceImpl.java
+++ src/main/java/com/moviefy/service/auth/AuthServiceImpl.java
@@ -1,12 +1,16 @@
 package com.moviefy.service.auth;
 
 import com.moviefy.database.model.dto.databaseDto.user.EmailVerificationTokenDTO;
+import com.moviefy.database.model.dto.databaseDto.user.PasswordResetConfirmDTO;
+import com.moviefy.database.model.dto.databaseDto.user.PasswordResetRequestDTO;
 import com.moviefy.database.model.dto.databaseDto.user.RegisterUserDTO;
 import com.moviefy.database.model.entity.user.AppUser;
 import com.moviefy.database.model.entity.user.EmailVerificationToken;
+import com.moviefy.database.model.entity.user.PasswordResetToken;
 import com.moviefy.database.model.entity.user.UserRole;
-import com.moviefy.database.repository.UserRepository;
+import com.moviefy.database.repository.user.UserRepository;
 import com.moviefy.exceptions.EmailAlreadyTakenException;
+import com.moviefy.exceptions.ExpiredPasswordResetTokenException;
 import com.moviefy.exceptions.ExpiredVerificationTokenException;
 import com.moviefy.exceptions.InvalidTokenException;
 import com.moviefy.service.auth.email.EmailService;
@@ -64,14 +68,14 @@ public class AuthServiceImpl implements AuthService {
         user.setRoles(roles);
         this.userRepository.save(user);
 
-        String token = this.emailService.generateToken(user);
+        String token = this.emailService.generateEmailVerificationToken(user);
         this.emailService.sendVerificationEmail(user.getEmail(), token);
     }
 
     @Override
     @Transactional
     public void verifyEmail(EmailVerificationTokenDTO emailVerificationTokenDTO) {
-        Optional<EmailVerificationToken> validToken = this.emailService.findValidToken(emailVerificationTokenDTO.getToken());
+        Optional<EmailVerificationToken> validToken = this.emailService.findValidEmailVerificationToken(emailVerificationTokenDTO.getToken());
 
         if (validToken.isEmpty()) {
             throw new InvalidTokenException("Invalid token.");
@@ -86,14 +90,14 @@ public class AuthServiceImpl implements AuthService {
         AppUser user = emailVerificationToken.getUser();
         user.setEmailVerified(true);
 
-        this.emailService.delete(emailVerificationToken);
+        this.emailService.deleteEmailVerificationTokens(emailVerificationToken);
         this.userRepository.save(user);
     }
 
     @Override
     @Transactional
-    public void resendEmail(EmailVerificationTokenDTO emailVerificationTokenDTO) {
-        Optional<EmailVerificationToken> optional = this.emailService.findToken(emailVerificationTokenDTO.getToken());
+    public void resendEmailVerification(EmailVerificationTokenDTO emailVerificationTokenDTO) {
+        Optional<EmailVerificationToken> optional = this.emailService.findEmailVerificationToken(emailVerificationTokenDTO.getToken());
 
         if (optional.isEmpty()) {
             throw new InvalidTokenException("Invalid optional.");
@@ -102,9 +106,43 @@ public class AuthServiceImpl implements AuthService {
         EmailVerificationToken emailVerificationToken = optional.get();
         String email = emailVerificationToken.getUser().getEmail();
 
-        String token = this.emailService.generateToken(emailVerificationToken.getUser());
+        String token = this.emailService.generateEmailVerificationToken(emailVerificationToken.getUser());
         this.emailService.sendVerificationEmail(email, token);
-        this.emailService.delete(emailVerificationToken);
+        this.emailService.deleteEmailVerificationTokens(emailVerificationToken);
+    }
+
+    @Override
+    public void requestPasswordReset(PasswordResetRequestDTO passwordResetRequestDTO) {
+        Optional<AppUser> byEmail = this.userRepository.findByEmail(passwordResetRequestDTO.getEmail());
+
+        if (byEmail.isEmpty()) {
+            return;
+        }
+        AppUser user = byEmail.get();
+
+        String token = this.emailService.generatePasswordResetToken(user);
+        this.emailService.sendPasswordResetEmail(user.getEmail(), token);
+    }
+
+    @Override
+    public void confirmPasswordReset(PasswordResetConfirmDTO passwordResetConfirmDTO) {
+        Optional<PasswordResetToken> validToken = this.emailService.findValidPasswordResetToken(passwordResetConfirmDTO.getToken());
+
+        if (validToken.isEmpty()) {
+            throw new InvalidTokenException("Invalid token.");
+        }
+
+        PasswordResetToken passwordResetToken = validToken.get();
+
+        if (passwordResetToken.getExpiresAt().isBefore(LocalDateTime.now())) {
+            throw new ExpiredPasswordResetTokenException("Password reset token has expired.");
+        }
+
+        AppUser user = passwordResetToken.getUser();
+        user.setPasswordHash(this.passwordEncoder.encode(passwordResetConfirmDTO.getPassword()));
+
+        this.emailService.deletePasswordResetToken(passwordResetToken);
+        this.userRepository.save(user);
     }
 
     private static String normalizeName(String s) {
