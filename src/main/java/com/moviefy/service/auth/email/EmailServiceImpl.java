2249dc21c727dbbbe09f478905d0f6f62ddf3bc3
AtanasRaev <atanaspraev@gmail.com>
2026-01-04 22:55:09 +0200

    Switch `EmailServiceImpl` to use HTML email templates for verification and password reset emails, add support for customizable logo URL.

--- src/main/java/com/moviefy/service/auth/email/EmailServiceImpl.java
+++ src/main/java/com/moviefy/service/auth/email/EmailServiceImpl.java
@@ -10,7 +10,6 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
