b344e83cd75cc9712e6ad94fc723c8a9ffac6199
AtanasRaev <atanaspraev@gmail.com>
2026-01-05 16:23:29 +0200

    Add `/me` endpoint to `AuthController` for retrieving authenticated user details and refactor response handling

--- src/main/java/com/moviefy/web/AuthController.java
+++ src/main/java/com/moviefy/web/AuthController.java
@@ -1,20 +1,26 @@
 package com.moviefy.web;
 
+import com.moviefy.database.model.dto.databaseDto.UserDTO;
 import com.moviefy.database.model.dto.databaseDto.user.*;
 import com.moviefy.database.model.dto.response.ApiResponse;
 import com.moviefy.service.auth.AuthService;
+import com.moviefy.service.auth.user.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService,
+                          UserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/register")
@@ -32,8 +38,7 @@ public class AuthController {
     @PostMapping("/verify-email")
     public ResponseEntity<ApiResponse<Void>> verifyEmail(@Valid @RequestBody EmailVerificationTokenDTO emailVerificationTokenDTO) {
         this.authService.verifyEmail(emailVerificationTokenDTO);
-        return ResponseEntity
-                .status(HttpStatus.OK)
+        return ResponseEntity.ok()
                 .body(ApiResponse.success(
                         HttpStatus.OK.value(),
                         "Email verified successfully.",
@@ -44,8 +49,7 @@ public class AuthController {
     @PostMapping("/resend-email")
     public ResponseEntity<ApiResponse<Void>> resendEmail(@Valid @RequestBody EmailVerificationTokenDTO emailVerificationTokenDTO) {
         this.authService.resendEmailVerification(emailVerificationTokenDTO);
-        return ResponseEntity
-                .status(HttpStatus.OK)
+        return ResponseEntity.ok()
                 .body(ApiResponse.success(
                         HttpStatus.OK.value(),
                         "Email verification link resent successfully.",
@@ -57,8 +61,7 @@ public class AuthController {
     public ResponseEntity<ApiResponse<Void>> changePassword(@Valid @RequestBody PasswordResetRequestDTO passwordResetRequestDTO) {
         this.authService.requestPasswordReset(passwordResetRequestDTO);
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
+        return ResponseEntity.ok()
                 .body(ApiResponse.success(
                         HttpStatus.OK.value(),
                         "If an account with that email exists, a password reset email has been sent.",
@@ -70,8 +73,7 @@ public class AuthController {
     public ResponseEntity<ApiResponse<Void>> checkPasswordResetToken(@Valid @RequestBody PasswordResetTokenCheckDTO passwordResetTokenCheckDTO) {
         this.authService.checkPasswordResetToken(passwordResetTokenCheckDTO);
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
+        return ResponseEntity.ok()
                 .body(ApiResponse.success(
                         HttpStatus.OK.value(),
                         "Token is valid.",
@@ -83,12 +85,29 @@ public class AuthController {
     public ResponseEntity<ApiResponse<Void>> confirmPasswordReset(@Valid @RequestBody PasswordResetConfirmDTO passwordResetConfirmDTO) {
         this.authService.confirmPasswordReset(passwordResetConfirmDTO);
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
+        return ResponseEntity.ok()
                 .body(ApiResponse.success(
                         HttpStatus.OK.value(),
                         "Password reset successful.",
                         null
+                ));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<UserDTO>> me(Authentication auth) {
+        if (auth == null || !auth.isAuthenticated() || "anonymousUser".equals(auth.getName())) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(ApiResponse.error(HttpStatus.UNAUTHORIZED.value(), "Unauthorized"));
+        }
+
+        UserDTO user = this.userService.getByEmail(auth.getName());
+
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(
+                        HttpStatus.OK.value(),
+                        "User is authenticated",
+                        user
                 ));
     }
 }
