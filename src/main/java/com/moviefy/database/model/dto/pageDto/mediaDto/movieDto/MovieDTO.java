01564f0d3990b96c7fd6663d3ca88a3fb8b9d3ba
AtanasRaev <atanaspraev@gmail.com>
2026-01-16 13:04:09 +0200

    Add `posterPath` field to `MovieDTO` and adjust `MoviePageDTO` accordingly.

--- src/main/java/com/moviefy/database/model/dto/pageDto/mediaDto/movieDto/MovieDTO.java
+++ src/main/java/com/moviefy/database/model/dto/pageDto/mediaDto/movieDto/MovieDTO.java
@@ -6,10 +6,14 @@ import com.moviefy.database.model.dto.pageDto.mediaDto.MediaPageDTO;
 public abstract class MovieDTO extends MediaPageDTO {
     private String title;
 
+    @JsonProperty("poster_path")
+    private String posterPath;
+
     private Integer runtime;
 
-    public MovieDTO(Long id, Double voteAverage, Double popularity, Long apiId, String title, Integer runtime) {
+    public MovieDTO(Long id, Double voteAverage, Double popularity, Long apiId, String title, Integer runtime, String posterPath) {
         super(id, voteAverage, popularity, apiId);
+        this.posterPath = posterPath;
         this.title = title;
         this.runtime = runtime;
     }
@@ -21,6 +25,14 @@ public abstract class MovieDTO extends MediaPageDTO {
     @JsonProperty("media_type")
     public String getMediaType() {
         return "movie";
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
     }
 
     public String getTitle() {
